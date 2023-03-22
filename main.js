1f3c5e218bfb0563fc79881ccb3bc665e2cf3c18
Anna Janíková <anna.janikova42@gmail.com>
2023-03-22 21:53:15 +0100

    vytvorena_sablona

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // tady je místo pro náš program
 
+const car = document.querySelector("#car")
 
-
+document.addEventListener("keydown", (e) => {
+	if (e.code === "39"){
+        car.style.marginLeft = `10px`
+    }
+    if (e.code === "37"){
+        car.style.marginLeft = "0px"
+    }
+})
 
